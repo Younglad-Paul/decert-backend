e2a3cb81bf43f0e5fb332c9d72d6d0f619508ca2
unknown <borisadedayopaul@gmail.com>
2024-01-10 18:12:13 +0100

    completed the user authentication and created the user access token

--- models/users.js
+++ models/users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const { isEmail} = require('validator')
+const bcrypt = require('bcrypt')
 
 const UsersSchema = new mongoose.Schema({
     profile: {
@@ -7,22 +9,39 @@ const UsersSchema = new mongoose.Schema({
     },
     firstname: {
         type: String,
-        required: true
+        required: [true, 'First name is required.']
     },
     lastname: {
         type: String,
-        required: true
+        required: [true, 'Last name is required.']
     },
     username: {
         type: String,
-        required: true
+        unique: true,
+        required: [true, 'Username is required!']
     },
     email: {
         type: String,
-        required: true
+        unique: true,
+        required: [true, 'Please input a valid email address!'],
+        validate: [isEmail, 'Please be sure that the email is valid.']
+    },
+    password: {
+        type: String
     }
 }, {
     timestamps: true
+});
+
+UsersSchema.pre('save', async function (next) {
+    console.log("Creating User in progress...");
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+});
+UsersSchema.post('save', function (doc, next) {
+    console.log("User Created successfully");
+    next();
 });
 
 const Users = mongoose.model('Users', UsersSchema);
--- server.js
+++ server.js
@@ -2,28 +2,78 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Users = require('./models/users');
+const userCookie = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
+app.use(userCookie())
 
 mongoose.set("strictQuery", false)
 
+const errorHandler = (err) => {
+    console.log(err.message, err.code)
+
+    let errors = {email: '', username: ''};
+
+    if (err.message.includes('Users validation failed')){
+        Object.values(err.errors).forEach(({properties}) =>{
+            errors [properties.path] = properties.message
+        })
+    }
+    return errors;
+}
+
+const maxAge = 2 * 24 * 60 * 60;
+
+AccessToken = (id) => {
+    return jwt.sign({id}, 'decertify user security hashing', {
+        expiresIn: maxAge
+    }) 
+}
+
 app.post('/users', async (req, res) => {
     try {
-        const users = await Users.create(req.body);
-        res.status(200).json(users);
+        const existingUser = await Users.findOne({ email: req.body.email });
+        const existingUserName = await Users.findOne({ username: req.body.username });
+        
+        if (existingUser) {
+            const errors = { email: 'Email is already registered' };
+            return res.status(400).json({ errors });
+        }
+
+        if (existingUserName) {
+            const errors = { username: 'Username already exist. Please create a unique username' };
+            return res.status(400).json({ errors });
+        }
+
+        const newUser = await Users.create(req.body);
+        const Token = AccessToken(newUser._id)
+        res.cookie('JWTAccessToken', Token, {httpOnly: true, maxAge: 1000 * 60 * 60 * 48})
+        res.status(200).json(newUser._id);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        const errors = errorHandler(error);
+        res.status(400).json({ errors });
     }
 });
 app.put('/user/:id', async (req, res) => {
     try {
-        const users = await Users.findbyId(id);
+        const Id = req.params.id;
+        const users = await Users.findById(Id);
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(404).json({ message: error.message });
+    }
+});
+app.delete('/user/:id', async (req, res) => {
+    try {
+        const Id = req.params.id;
+        const deletedUser = await Users.findByIdAndDelete(Id);
+        res.status(200).json(deletedUser);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 });
 
